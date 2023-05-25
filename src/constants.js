d7ca9af32f32a10e89a6f4b06be3d0ae7d9642f8
VedBhanushali <19ce005@charusat.edu.in>
2023-05-26 02:08:51 +0530

    Readme file updated

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,2 @@
-//this file can be config or constants as per developer choice
-//here we will do name import of all constants
-
 export const JSON_API = "https://jsonplaceholder.typicode.com";
 
--- utils/userPosts.jsx
+++ utils/userPosts.jsx
@@ -1,8 +1,5 @@
 import {useState,useEffect} from 'react';
 import {JSON_API} from '../src/constants';
-//creating Hooks
-// this will fetch the data and send it to component for render in RestaurantMenu
-//Hooks returns some data - it doesn't return jsx.
 
 const userPosts = (user_id) => {
     const [userPost,setUserPost] = useState(null); 
